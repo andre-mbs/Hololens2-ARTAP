032e08cd38c8b3ff3096774703bc874f8d58a007
André Santos <andre.mb.santos@gmail.com>
2022-03-28 20:28:15 +0100

    Add box label that changes orientation depending on users head

--- Assets/Scripts/BoxTagInformation.cs
+++ Assets/Scripts/BoxTagInformation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Microsoft.MixedReality.Toolkit.UI;
 
 public class BoxTagInformation : MonoBehaviour
 {
@@ -9,18 +10,18 @@ public class BoxTagInformation : MonoBehaviour
     public string partReference;
     public string partLocation;
 
+    public GameObject toolTip;
+
     public bool tagSet;
 
 	// Start is called before the first frame update
 	void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void UpdateInfo(string partName, string partReference, string partLocation)
@@ -28,5 +29,15 @@ public class BoxTagInformation : MonoBehaviour
         this.partName = partName;
         this.partReference = partReference;
         this.partLocation = partLocation;
+
+        toolTip.GetComponent<ToolTip>().ToolTipText = partReference;
     }
+
+	public void EnableTooltip()
+	{
+		if (tagSet)
+		{
+            toolTip.SetActive(true);
+        }
+	}
 }
